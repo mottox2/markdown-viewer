cc18de57ce85c1207c2e19448811bfaa22d774d8
mottox2 <y.mottox2@gmail.com>
2016-12-21 14:30:09 +0900

    Add marked sample

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react'
 import CSSModules from 'react-css-modules'
+import marked  from 'marked'
 
 import styles from './App.css'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      value: '',
+    }
+  }
+
+  onChange(value) {
+    this.setState({value})
+  }
+
   render() {
+    const html = marked(this.state.value, {
+      breaks: true,
+    })
     return <div>
       <p styleName='hoge'>This is React Component</p>
+      <textarea styleName='textarea' value={this.state.value} onChange={(e) => this.onChange(e.target.value)}/>
+      <div dangerouslySetInnerHTML={{__html: html}}/>
+      <div>{html}</div>
     </div>
   }
 }
